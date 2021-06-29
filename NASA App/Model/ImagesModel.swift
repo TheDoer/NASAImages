0b5e42288c762ae5441bc48a96d84dc834cef75a
Adonis <adonis.rumbwere@cassavasmartech.co.zw>
2021-06-29 12:48:01 +0200

    initial commit

---
+++ NASA App/Model/ImagesModel.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesModel.swift
+//  NASA App
+//
+//  Created by Adonis Rumbwere on 28/6/2021.
+//
+
+import Foundation
+
+struct ImagesData {
+    
+}
+
---
+++ NASA App/ViewModel/Parser.swift
@@ -0,0 +1,8 @@
+//
+//  Parser.swift
+//  NASA App
+//
+//  Created by Adonis Rumbwere on 28/6/2021.
+//
+
+import Foundation
