d5f1e21bee19cbee07bfa32fb33e45be11afd0c0
Adonis <adonis.rumbwere@cassavasmartech.co.zw>
2021-07-01 07:21:19 +0200

    webService

--- NASA App/WebService/APIService.swift
+++ NASA App/WebService/APIService.swift
@@ -7,20 +7,56 @@
 
 import Foundation
 
-class APIService: NSObject {
+class APIService {
     
-    private let url = URL(string: "https://images-api.nasa.gov/search?q=%22%22")!
+    private var dataTask: URLSessionDataTask?
     
-    func getImagesDataApi(completion: @escaping (CollectionData)-> ()){
+    func getImagesData(completion: @escaping(Result<CollectionData, Error>) -> Void) {
         
-        URLSession.shared.dataTask(with: url){ data , response, error in
+        let nasaImagesUrl = "https://images-api.nasa.gov/search?q=%22%22"
+        
+        guard let url = URL(string: nasaImagesUrl) else { return }
+        
+        //Create URL Session - work on the background
+        
+        dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
             
-            if let data = data {
-                let json = try! JSONDecoder().decode(CollectionData.self, from: data)
-                print(json)
-                completion(json)
+            //Handle Error
+            
+            if let error = error {
+                completion(.failure(error))
+                print("DataTask error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                //Handle Empty Response
+                return
+            }
+            
+            print("Response status code: \(response.statusCode)")
+            
+            guard let data = data else {
+                //Handle Empty Data
+                print("Empty Data")
+                return
+            }
+            
+            do {
+                //Parse the data
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(CollectionData.self, from: data)
                 
+                //Back to the mnain thread
+                DispatchQueue.main.async {
+                    completion(.success(jsonData))
+                }
+            } catch let error {
+                completion(.failure(error))
             }
         }
+        dataTask?.resume()
+        
     }
+      
 }
