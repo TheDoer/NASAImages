d6d98b17098b908d25f9f1342f63416ca34853a1
Adonis <adonis.rumbwere@cassavasmartech.co.zw>
2021-07-01 16:03:24 +0200

    rxSwift binding table data

--- NASA App/Controller/ViewController.swift
+++ NASA App/Controller/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
@@ -13,6 +15,7 @@ class ViewController: UIViewController {
     //var apiService = APIService()
     
     private var viewModel = ImagesViewModel()
+    private var bag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,31 +25,77 @@ class ViewController: UIViewController {
 //           print(result)
 //       }
         
+        
         loadImagesData()
+        bindTableData()
+    }
+    
+    func bindTableData(){
+        
+        //Bind item to table
+        ImagesViewModel.items.bind(
+            to: tableView.rx.items(
+                 cellIdentifier: "cell",
+                 cellType: ImagesTableViewCell.self)
+          ) { row , item , cell in
+            
+            //let json = viewModel.cellForAt(indexPath: indexPath)
+            //print("JSonData: \(json)")
+            //cell.setCellWithValueOf(json)
+            
+
+        }.disposed(by: bag)
+        
+
+
+        //Bind a model selected handler
+        tableView.rx.modelSelected(Item.self).bind { item in
+        }.disposed(by: bag)
+           
+//
+//
+        //fetch items
+        viewModel.fetchImagesData {
+            completion()
+        }
+        
     }
     
     private func loadImagesData(){
         viewModel.fetchImagesData { [weak self] in
-            self?.tableView.dataSource = self
-            self?.tableView.reloadData()
+            
+            
+
         }
     }
+    
 }
 
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numberOfRowsInSection(section: section)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ImagesTableViewCell
-        
-        let json = viewModel.cellForAt(indexPath: indexPath)
-        print("JSonData: \(json)")
-        cell.setCellWithValueOf(<#Data#>)
-        
-        return cell
-    }
-    
-    
-}
+
+
+//extension ViewController: UITableViewDataSource, UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        //return viewModel.numberOfRowsInSection(section: section)
+//
+//        return 5
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ImagesTableViewCell
+//
+//        //let json = viewModel.cellForAt(indexPath: indexPath)
+//        //print("JSonData: \(json)")
+//        //cell.setCellWithValueOf(json)
+//
+//
+//
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//
+//        return 120
+//    }
+//
+//
+//}
