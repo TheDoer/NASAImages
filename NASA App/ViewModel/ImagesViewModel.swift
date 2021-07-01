992921162dfb96222ba0170f027ff044f0737283
Adonis <adonis.rumbwere@cassavasmartech.co.zw>
2021-07-01 07:01:27 +0200

    fetching nasa images and populating view model

--- NASA App/ViewModel/ImagesViewModel.swift
+++ NASA App/ViewModel/ImagesViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class ImagesViewModel {
+    
+    private var apiService = APIService()
+    private var nasaImages = [Item]()
+    
+    
+    func fetchImagesData(completion: @escaping () -> ()) {
+        
+        //weak self - prevent retain cycles
+        
+        apiService.getImagesData { (result) in
+            switch result {
+                case .success(let listOf):
+                    self.nasaImages = (listOf.collection.items)!
+                    print("NASA ImageData: \(self.nasaImages)")
+                    completion()
+                case .failure(let error):
+                    print("Error processing json data:\(error)")
+            }
+        }
+    }
+    
+    func numberOfRowsInSection(section: Int) -> Int {
+        if nasaImages.count != 0 {
+            return nasaImages.count
+        }
+        
+        return 0
+    }
+    
+    func cellForAt (indexPath: IndexPath) -> Item {
+        return nasaImages[indexPath.row]
+    }
+}
